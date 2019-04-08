884fdaa9d069180890276ae93eeb4e23505e2fa5
OH SOUNG KWON <crystals8960@gmail.com>
2019-04-08 15:02:01 +0900

    Ch04_04

---
+++ Ch04_03/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Ch04_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSrc = new System.Windows.Forms.Button();
+            this.btnDest = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txtSrc = new System.Windows.Forms.TextBox();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.lblSrc = new System.Windows.Forms.Label();
+            this.lblDest = new System.Windows.Forms.Label();
+            this.gbBox = new System.Windows.Forms.GroupBox();
+            this.rbCopy = new System.Windows.Forms.RadioButton();
+            this.rbMove = new System.Windows.Forms.RadioButton();
+            this.lvDest = new System.Windows.Forms.ListView();
+            this.lvSrc = new System.Windows.Forms.ListView();
+            this.fbdFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.ssBar = new System.Windows.Forms.StatusStrip();
+            this.chFileSrc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chFileDest = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tsslbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tspgrbar = new System.Windows.Forms.ToolStripProgressBar();
+            this.tsslblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.gbBox.SuspendLayout();
+            this.ssBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSrc
+            // 
+            this.btnSrc.Location = new System.Drawing.Point(389, 12);
+            this.btnSrc.Name = "btnSrc";
+            this.btnSrc.Size = new System.Drawing.Size(75, 23);
+            this.btnSrc.TabIndex = 0;
+            this.btnSrc.Text = "대상 경로";
+            this.btnSrc.UseVisualStyleBackColor = true;
+            this.btnSrc.Click += new System.EventHandler(this.btnSrc_Click);
+            // 
+            // btnDest
+            // 
+            this.btnDest.Location = new System.Drawing.Point(389, 39);
+            this.btnDest.Name = "btnDest";
+            this.btnDest.Size = new System.Drawing.Size(75, 23);
+            this.btnDest.TabIndex = 1;
+            this.btnDest.Text = "결과 경로";
+            this.btnDest.UseVisualStyleBackColor = true;
+            this.btnDest.Click += new System.EventHandler(this.btnDest_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(710, 11);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(123, 48);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "실행";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // txtSrc
+            // 
+            this.txtSrc.Location = new System.Drawing.Point(12, 12);
+            this.txtSrc.Name = "txtSrc";
+            this.txtSrc.ReadOnly = true;
+            this.txtSrc.Size = new System.Drawing.Size(371, 21);
+            this.txtSrc.TabIndex = 3;
+            // 
+            // txtDest
+            // 
+            this.txtDest.Location = new System.Drawing.Point(12, 39);
+            this.txtDest.Name = "txtDest";
+            this.txtDest.ReadOnly = true;
+            this.txtDest.Size = new System.Drawing.Size(371, 21);
+            this.txtDest.TabIndex = 4;
+            // 
+            // lblSrc
+            // 
+            this.lblSrc.AutoSize = true;
+            this.lblSrc.Location = new System.Drawing.Point(10, 73);
+            this.lblSrc.Name = "lblSrc";
+            this.lblSrc.Size = new System.Drawing.Size(29, 12);
+            this.lblSrc.TabIndex = 5;
+            this.lblSrc.Text = "대상";
+            // 
+            // lblDest
+            // 
+            this.lblDest.AutoSize = true;
+            this.lblDest.Location = new System.Drawing.Point(424, 73);
+            this.lblDest.Name = "lblDest";
+            this.lblDest.Size = new System.Drawing.Size(29, 12);
+            this.lblDest.TabIndex = 6;
+            this.lblDest.Text = "결과";
+            // 
+            // gbBox
+            // 
+            this.gbBox.Controls.Add(this.rbMove);
+            this.gbBox.Controls.Add(this.rbCopy);
+            this.gbBox.Location = new System.Drawing.Point(495, 12);
+            this.gbBox.Name = "gbBox";
+            this.gbBox.Size = new System.Drawing.Size(209, 49);
+            this.gbBox.TabIndex = 7;
+            this.gbBox.TabStop = false;
+            this.gbBox.Text = "선택";
+            // 
+            // rbCopy
+            // 
+            this.rbCopy.AutoSize = true;
+            this.rbCopy.Location = new System.Drawing.Point(6, 20);
+            this.rbCopy.Name = "rbCopy";
+            this.rbCopy.Size = new System.Drawing.Size(71, 16);
+            this.rbCopy.TabIndex = 0;
+            this.rbCopy.TabStop = true;
+            this.rbCopy.Text = "파일복사";
+            this.rbCopy.UseVisualStyleBackColor = true;
+            // 
+            // rbMove
+            // 
+            this.rbMove.AutoSize = true;
+            this.rbMove.Location = new System.Drawing.Point(104, 20);
+            this.rbMove.Name = "rbMove";
+            this.rbMove.Size = new System.Drawing.Size(71, 16);
+            this.rbMove.TabIndex = 1;
+            this.rbMove.TabStop = true;
+            this.rbMove.Text = "파일이동";
+            this.rbMove.UseVisualStyleBackColor = true;
+            // 
+            // lvDest
+            // 
+            this.lvDest.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chFileDest});
+            this.lvDest.GridLines = true;
+            this.lvDest.Location = new System.Drawing.Point(426, 88);
+            this.lvDest.Name = "lvDest";
+            this.lvDest.Size = new System.Drawing.Size(407, 331);
+            this.lvDest.TabIndex = 2;
+            this.lvDest.UseCompatibleStateImageBehavior = false;
+            this.lvDest.View = System.Windows.Forms.View.Details;
+            // 
+            // lvSrc
+            // 
+            this.lvSrc.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chFileSrc});
+            this.lvSrc.GridLines = true;
+            this.lvSrc.Location = new System.Drawing.Point(12, 88);
+            this.lvSrc.Name = "lvSrc";
+            this.lvSrc.Size = new System.Drawing.Size(407, 331);
+            this.lvSrc.TabIndex = 3;
+            this.lvSrc.UseCompatibleStateImageBehavior = false;
+            this.lvSrc.View = System.Windows.Forms.View.Details;
+            // 
+            // ssBar
+            // 
+            this.ssBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslbl,
+            this.tspgrbar,
+            this.tsslblStatus});
+            this.ssBar.Location = new System.Drawing.Point(0, 428);
+            this.ssBar.Name = "ssBar";
+            this.ssBar.Size = new System.Drawing.Size(846, 22);
+            this.ssBar.TabIndex = 8;
+            this.ssBar.Text = "statusStrip1";
+            // 
+            // chFileSrc
+            // 
+            this.chFileSrc.Text = "파일";
+            this.chFileSrc.Width = 400;
+            // 
+            // chFileDest
+            // 
+            this.chFileDest.Text = "결과";
+            this.chFileDest.Width = 400;
+            // 
+            // tsslbl
+            // 
+            this.tsslbl.Name = "tsslbl";
+            this.tsslbl.Size = new System.Drawing.Size(90, 17);
+            this.tsslbl.Text = "전체 진행사항 :";
+            // 
+            // tspgrbar
+            // 
+            this.tspgrbar.Name = "tspgrbar";
+            this.tspgrbar.Size = new System.Drawing.Size(200, 16);
+            // 
+            // tsslblStatus
+            // 
+            this.tsslblStatus.Name = "tsslblStatus";
+            this.tsslblStatus.Size = new System.Drawing.Size(28, 17);
+            this.tsslblStatus.Text = "0 %";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 450);
+            this.Controls.Add(this.ssBar);
+            this.Controls.Add(this.lvDest);
+            this.Controls.Add(this.lvSrc);
+            this.Controls.Add(this.gbBox);
+            this.Controls.Add(this.lblDest);
+            this.Controls.Add(this.lblSrc);
+            this.Controls.Add(this.txtDest);
+            this.Controls.Add(this.txtSrc);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.btnDest);
+            this.Controls.Add(this.btnSrc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "파일 복사/이동";
+            this.gbBox.ResumeLayout(false);
+            this.gbBox.PerformLayout();
+            this.ssBar.ResumeLayout(false);
+            this.ssBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSrc;
+        private System.Windows.Forms.Button btnDest;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtSrc;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.Label lblSrc;
+        private System.Windows.Forms.Label lblDest;
+        private System.Windows.Forms.GroupBox gbBox;
+        private System.Windows.Forms.RadioButton rbMove;
+        private System.Windows.Forms.RadioButton rbCopy;
+        private System.Windows.Forms.ListView lvDest;
+        private System.Windows.Forms.ListView lvSrc;
+        private System.Windows.Forms.FolderBrowserDialog fbdFolder;
+        private System.Windows.Forms.StatusStrip ssBar;
+        private System.Windows.Forms.ColumnHeader chFileDest;
+        private System.Windows.Forms.ColumnHeader chFileSrc;
+        private System.Windows.Forms.ToolStripStatusLabel tsslbl;
+        private System.Windows.Forms.ToolStripProgressBar tspgrbar;
+        private System.Windows.Forms.ToolStripStatusLabel tsslblStatus;
+    }
+}
+
---
+++ Ch04_04/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Ch04_04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPath = new System.Windows.Forms.Label();
+            this.lblWipe = new System.Windows.Forms.Label();
+            this.lblPgb = new System.Windows.Forms.Label();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.btnWipe = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.cbWipe = new System.Windows.Forms.ComboBox();
+            this.pgbBar = new System.Windows.Forms.ProgressBar();
+            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(10, 10);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(37, 12);
+            this.lblPath.TabIndex = 0;
+            this.lblPath.Text = "파일 :";
+            // 
+            // lblWipe
+            // 
+            this.lblWipe.AutoSize = true;
+            this.lblWipe.Location = new System.Drawing.Point(10, 37);
+            this.lblWipe.Name = "lblWipe";
+            this.lblWipe.Size = new System.Drawing.Size(37, 12);
+            this.lblWipe.TabIndex = 1;
+            this.lblWipe.Text = "선택 :";
+            // 
+            // lblPgb
+            // 
+            this.lblPgb.AutoSize = true;
+            this.lblPgb.Location = new System.Drawing.Point(18, 75);
+            this.lblPgb.Name = "lblPgb";
+            this.lblPgb.Size = new System.Drawing.Size(21, 12);
+            this.lblPgb.TabIndex = 2;
+            this.lblPgb.Text = "0%";
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(398, 5);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(25, 23);
+            this.btnPath.TabIndex = 3;
+            this.btnPath.Text = "...";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // btnWipe
+            // 
+            this.btnWipe.Location = new System.Drawing.Point(429, 6);
+            this.btnWipe.Name = "btnWipe";
+            this.btnWipe.Size = new System.Drawing.Size(54, 47);
+            this.btnWipe.TabIndex = 4;
+            this.btnWipe.Text = "삭제";
+            this.btnWipe.UseVisualStyleBackColor = true;
+            this.btnWipe.Click += new System.EventHandler(this.btnWipe_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(55, 6);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(337, 21);
+            this.txtPath.TabIndex = 5;
+            // 
+            // cbWipe
+            // 
+            this.cbWipe.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbWipe.FormattingEnabled = true;
+            this.cbWipe.Items.AddRange(new object[] {
+            "British HMG IS5 (Base Line)",
+            "British HMG IS5 (Enhanced)"});
+            this.cbWipe.Location = new System.Drawing.Point(55, 33);
+            this.cbWipe.Name = "cbWipe";
+            this.cbWipe.Size = new System.Drawing.Size(368, 20);
+            this.cbWipe.TabIndex = 6;
+            // 
+            // pgbBar
+            // 
+            this.pgbBar.Location = new System.Drawing.Point(55, 70);
+            this.pgbBar.Name = "pgbBar";
+            this.pgbBar.Size = new System.Drawing.Size(428, 23);
+            this.pgbBar.TabIndex = 7;
+            // 
+            // ofdFile
+            // 
+            this.ofdFile.FileName = "openFileDialog1";
+            this.ofdFile.Filter = "모든 파일 (*.*)|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 102);
+            this.Controls.Add(this.pgbBar);
+            this.Controls.Add(this.cbWipe);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnWipe);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.lblPgb);
+            this.Controls.Add(this.lblWipe);
+            this.Controls.Add(this.lblPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "파일 지우기";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Label lblWipe;
+        private System.Windows.Forms.Label lblPgb;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.Button btnWipe;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.ComboBox cbWipe;
+        private System.Windows.Forms.ProgressBar pgbBar;
+        private System.Windows.Forms.OpenFileDialog ofdFile;
+    }
+}
+
